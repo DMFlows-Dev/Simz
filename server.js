09503bd8d6fc6ff68a6bcf54b09b57a8e81aacfb
DMFlows-Dev <dm.muhammadaarib@gmail.com>
2025-10-14 14:28:18 +0500

    update env

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ app.use(express.json());
 app.use('/api', callRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Call Analytics API');
+  res.send({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
 });
 
 // Global error handler. 
